19af5e5d5f91236dce4873a96ad8a454c0e5c3b4
Thomas Texier <thomas_texier@connect-tech.sncf>
2024-06-11 14:35:23 +0200

    [INIT] Init api-clients JWT

--- api-clients/App/Manager/Security/AuthorizationManager.php
+++ api-clients/App/Manager/Security/AuthorizationManager.php
@@ -2,12 +2,16 @@
 
 namespace Clients\Manager\Security;
 
+use Clients\Manager\Env\EnvManager;
 use Clients\Manager\Error\RequestsError;
 use Clients\Manager\Error\RequestsErrorsTypes;
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+use LogicException;
+use UnexpectedValueException;
 
 class AuthorizationManager
 {
-
     public static function handleAuthorization(): void
     {
 
@@ -20,7 +24,27 @@ class AuthorizationManager
 
     private static function isAuthorized(): bool
     {
-        return true; //TODO JWT CHECK¬
+        $key = EnvManager::getInstance()->getValue('JWT_KEY');
+        $payload = [
+            'iss' => 'API Clients',
+            'iat' => time(), //Date de génération du token
+            'exp' => time() + 60,
+        ];
+
+        $tokenJwt = JWT::encode($payload, $key, 'HS256');
+        var_dump($tokenJwt);
+
+        try {
+            $decoded = JWT::decode($tokenJwt, new Key($key, 'HS256'));
+
+            print_r($decoded);
+        } catch (LogicException $e) {
+            // errors having to do with environmental setup or malformed JWT Keys
+        } catch (UnexpectedValueException $e) {
+            // errors having to do with JWT signature and claims
+        }
+
+        return true;
     }
 
 }
