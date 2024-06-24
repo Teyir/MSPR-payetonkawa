9b081adbb463a2c9805932f721cae213d98eac9d
Teyir <60007549+Teyir@users.noreply.github.com>
2024-06-24 14:53:55 +0200

    [CI] Setup API CI tests

--- api-customers/Tests/Manager/Database/DatabaseManagerTest.php
+++ api-customers/Tests/Manager/Database/DatabaseManagerTest.php
@@ -8,12 +8,6 @@ use PHPUnit\Framework\TestCase;
 
 class DatabaseManagerTest extends TestCase
 {
-    protected function setUp(): void
-    {
-        require_once "test_loader.php";
-        parent::setUp();
-    }
-
 
     #[TestDox('Test PDO generation.')]
     public function testGetPDO(): void
