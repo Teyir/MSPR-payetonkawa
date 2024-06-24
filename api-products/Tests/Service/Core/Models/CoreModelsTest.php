58d4e1850b1f329b9aa9e8536e7e812a48ea5786
Teyir <60007549+Teyir@users.noreply.github.com>
2024-06-24 16:58:27 +0200

    [CI] test coverage

--- api-products/Tests/Service/Core/Models/CoreModelsTest.php
+++ api-products/Tests/Service/Core/Models/CoreModelsTest.php
@@ -4,12 +4,20 @@ namespace Service\Core\Models;
 
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\Attributes\UsesClass;
+use Products\Manager\Class\GlobalObject;
+use Products\Manager\Database\DatabaseManager;
+use Products\Manager\Env\EnvManager;
+use Products\Manager\Loader\AutoLoad;
 use Products\Model\Core\CoreModels;
 use PHPUnit\Framework\Attributes\TestDox;
 use PHPUnit\Framework\TestCase;
 
 #[CoversClass(CoreModels::class)]
 #[UsesClass(CoreModels::class)]
+#[UsesClass(GlobalObject::class)]
+#[UsesClass(DatabaseManager::class)]
+#[UsesClass(EnvManager::class)]
+#[UsesClass(AutoLoad::class)]
 class CoreModelsTest extends TestCase
 {
     private CoreModels $model;
