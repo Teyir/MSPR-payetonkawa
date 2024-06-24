cb2026a4b115b1067effdf49686fc368a8fafe75
Teyir <60007549+Teyir@users.noreply.github.com>
2024-06-24 16:56:40 +0200

    [CI] test coverage

--- api-products/Tests/Manager/Database/DatabaseManagerTest.php
+++ api-products/Tests/Manager/Database/DatabaseManagerTest.php
@@ -7,9 +7,13 @@ use PHPUnit\Framework\Attributes\TestDox;
 use PHPUnit\Framework\Attributes\UsesClass;
 use PHPUnit\Framework\TestCase;
 use Products\Manager\Database\DatabaseManager;
+use Products\Manager\Env\EnvManager;
+use Products\Manager\Loader\AutoLoad;
 
 #[CoversClass(DatabaseManager::class)]
 #[UsesClass(DatabaseManager::class)]
+#[UsesClass(EnvManager::class)]
+#[UsesClass(AutoLoad::class)]
 class DatabaseManagerTest extends TestCase
 {
 
