c2325e68ba22c7c4eced3e2d965ffe86d3fa3aee
HowBaka <gravy.thom@gmail.com>
2024-06-25 08:57:12 +0200

    [IMPROVES] New wording HP

--- web/Public/Themes/Default/Views/Core/home.view.php
+++ web/Public/Themes/Default/Views/Core/home.view.php
@@ -9,6 +9,6 @@ Website::setDescription("Page d'accueil PayeTonKawa.fr");
 
 <div class="bg-zinc-100 h-screen w-screen flex flex-col justify-center items-center space-y-2">
     <h1 class="text-4xl tracking-tighter font-bold">Bienvenue sur payetonkawa.fr</h1>
-    <p class="font-semibold">Découvrez les cafés les plus <span class="font-black tracking-widest text-orange-600">chauds</span> de ta région</p>
+    <p class="font-semibold">Découvrez les kawa les plus <span class="font-black tracking-widest text-orange-600">chauds</span> de vos régions</p>
     <a class="btn" href="/web/products">Slurp</a>
 </div>
